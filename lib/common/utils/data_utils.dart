1361e2563b7dfee73f837775d3005b96a43a22c2
yeongwoo <jyy0223@naver.com>
2024-05-03 13:48:45 +0900

    [feat] DataUtils 정의: 날짜 형식 변경, uuid 생성기

---
+++ lib/common/utils/data_utils.dart
@@ -0,0 +1,23 @@
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+class DataUtils {
+  static String convertDateTimeToDateString({
+    required DateTime datetime,
+  }) {
+    return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')}';
+  }
+
+  static String convertPriceToMoneyString({
+    required int price,
+  }) {
+    final NumberFormat numberFormat = NumberFormat('###,###,###,###');
+
+    return numberFormat.format(price);
+  }
+
+  static String getUuid() {
+    final uuid = Uuid();
+    return uuid.v4();
+  }
+}
