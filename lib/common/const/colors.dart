747aa239940dd89cbb1c24a2af41f0d4b12c19bb
yeongwoo <jyy0223@naver.com>
2024-05-03 13:36:11 +0900

    [feat] const colors, text_style, button_style

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  // Main
+  static const primary = Color(0xFFFF5A36);
+  static const secondary = Color(0xFFFF9A85);
+  static const third = Color(0xFFB42001);
+
+  // Text Color
+  static const text = Color(0xFF1C1C1E);
+  static const white = Color(0xFFFDFDFD);
+  static const error = Color(0xFFFF555F);
+  static const notice = Color(0xFFEBF4FF);
+
+  static const darkGrey = Color(0xFF767676);
+  static const middleGrey = Color(0xFFE5E5EA);
+  static const lightGrey = Color(0xFFF4F5F9);
+  static const barrier = Color(0x111C1C1E);
+}
