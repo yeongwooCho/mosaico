dff9bd94aef35a4a62f22ad07776673d7c8a6d0d
yeongwoo <jyy0223@naver.com>
2024-05-03 13:50:12 +0900

    [feat] go_router 정의

---
+++ lib/common/router/go_router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mosaico/common/view/error_screen.dart';
+import 'package:mosaico/common/view/splash_screen.dart';
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
+final router = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  redirect: null,
+  initialLocation: '/splash',
+  routes: routes,
+  errorBuilder: (context, state) => ErrorScreen(
+    error: state.error.toString(),
+  ),
+  debugLogDiagnostics: true,
+);
+
+List<RouteBase> get routes => [
+      GoRoute(
+        path: '/splash',
+        name: SplashScreen.routeName,
+        builder: (context, state) => SplashScreen(),
+      )
+    ];
