1478f0ed84648bc15a09de6b303872b1095fca4a
yeongwoo <jyy0223@naver.com>
2024-05-04 20:33:15 +0900

    [feat] 공지사항 모델 정의

---
+++ lib/notification/model/notification_model.dart
@@ -0,0 +1,13 @@
+class NotificationModel {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime createAt;
+
+  NotificationModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createAt,
+  });
+}
