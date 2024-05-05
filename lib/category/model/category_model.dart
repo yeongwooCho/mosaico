b9c6dde5c80cacaee5926fdb3912f1bda1af6889
yeongwoo <jyy0223@naver.com>
2024-05-05 14:11:22 +0900

    [feat] CategoryModel 선언

---
+++ lib/category/model/category_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  final Widget icon;
+  final String key;
+  final List<String> values;
+
+  CategoryModel({
+    required this.icon,
+    required this.key,
+    required this.values,
+  });
+}
