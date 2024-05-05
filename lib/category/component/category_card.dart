d7f371547936bd978ee02c35b36f8b1ce901f7a3
yeongwoo <jyy0223@naver.com>
2024-05-05 14:12:35 +0900

    [feat] CategoryCard 구현: 버튼 클릭시 등장하는 AnimatedContainer CategoryCard 에 Visible Widget으로 같이 구현

--- lib/category/component/category_card.dart
+++ lib/category/component/category_card.dart
@@ -1,29 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:mosaico/category/model/category_model.dart';
 import 'package:mosaico/common/const/colors.dart';
+import 'package:mosaico/common/const/text_styles.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
-class CategoryCard extends StatelessWidget {
-  final String title;
+class CategoryCard extends StatefulWidget {
+  final CategoryModel category;
 
   const CategoryCard({
     super.key,
-    required this.title,
+    required this.category,
   });
 
   @override
+  State<CategoryCard> createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard> {
+  bool isVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: MyColor.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24.0,
-          vertical: 16.0,
+    // width
+    final double fullWidth = MediaQuery.of(context).size.width;
+
+    // Container height
+    final values = widget.category.values.length;
+    final int phase = values % 2 == 0 ? values ~/ 2 : values ~/ 2 + 1;
+    final double containerHeight = (phase * 48) + ((phase - 1) * 8) + (16 * 2);
+
+    return Column(
+      children: [
+        InkWell(
+          onTap: () {
+            setState(() {
+              isVisible = !isVisible;
+            });
+          },
+          child: Container(
+            color: MyColor.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24.0,
+                vertical: 16.0,
+              ),
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        widget.category.icon,
+                        const SizedBox(width: 12.0),
+                        Text(
+                          widget.category.key,
+                          style: MyTextStyle.bodyTitleMedium,
+                        ),
+                      ],
+                    ),
+                    PhosphorIcon(
+                      isVisible
+                          ? PhosphorIcons.caretUp()
+                          : PhosphorIcons.caretDown(),
+                      size: 28.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
-        child: Row(
-          children: [
-            Text(title),
-          ],
+        AnimatedContainer(
+          height: isVisible ? containerHeight : 0.0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.fastOutSlowIn,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              children: List.generate(
+                isVisible ? widget.category.values.length : 0,
+                (index) => Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.0,
+                      color: MyColor.text,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  width: (fullWidth - 48 - 8) / 2,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Center(
+                      child: Text(
+                        widget.category.values[index],
+                        style: MyTextStyle.descriptionRegular,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
