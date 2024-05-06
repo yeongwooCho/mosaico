3acb094292a3f54d21aa3af9f7061b4d7772c7f6
yeongwoo <jyy0223@naver.com>
2024-05-06 21:36:42 +0900

    [feat] 이벤트 vertical list 를 컴포넌트로 분기

---
+++ lib/event/component/event_main_vertival_list.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mosaico/event/component/event_card.dart';
+import 'package:mosaico/event/model/event_model.dart';
+import 'package:mosaico/event/view/event_detail_screen.dart';
+
+class EventMainVerticalList extends StatelessWidget {
+  final List<EventModel> events;
+
+  const EventMainVerticalList({
+    super.key,
+    required this.events,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        final event = events[index];
+
+        return GestureDetector(
+          onTap: () {
+            context.goNamed(
+              EventDetailScreen.routeName,
+              pathParameters: {'id': event.id},
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 16.0,
+            ),
+            child: EventCard.fromModel(model: event),
+          ),
+        );
+      },
+      itemCount: events.length,
+    );
+  }
+}
--- lib/event/view/event_screen.dart
+++ lib/event/view/event_screen.dart
@@ -4,9 +4,8 @@ import 'package:go_router/go_router.dart';
 import 'package:mosaico/common/const/image_path.dart';
 import 'package:mosaico/common/layout/default_app_bar.dart';
 import 'package:mosaico/common/layout/default_layout.dart';
-import 'package:mosaico/event/component/event_card.dart';
+import 'package:mosaico/event/component/event_main_vertival_list.dart';
 import 'package:mosaico/event/provider/event_provider.dart';
-import 'package:mosaico/event/view/event_detail_screen.dart';
 import 'package:mosaico/notification/view/notification_screen.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
@@ -38,29 +37,7 @@ class EventScreen extends ConsumerWidget {
           ),
         ],
       ),
-      child: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          final event = events[index];
-
-          return GestureDetector(
-            onTap: () {
-              context.goNamed(
-                EventDetailScreen.routeName,
-                pathParameters: {'id': event.id},
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 16.0,
-              ),
-              child: EventCard.fromModel(model: event),
-            ),
-          );
-        },
-        itemCount: events.length,
-      ),
+      child: EventMainVerticalList(events: events),
     );
   }
 }
